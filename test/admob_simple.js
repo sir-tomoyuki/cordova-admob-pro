2485a461f0f6c7bffe29f76baa3d4c7cf62967e4
sir-tomoyuki <piq-soho2@mail.goo.ne.jp>
2017-03-13 13:23:11 +0900

    Update admob_simple.js

--- test/admob_simple.js
+++ test/admob_simple.js
@@ -3,18 +3,18 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) { 
   admobid = { // for Android
-    banner: 'ca-app-pub-6869992474017983/9375997553',
-    interstitial: 'ca-app-pub-6869992474017983/1657046752'
+    banner: 'ca-app-pub-4498940312772097/6475832156',
+    interstitial: 'ca-app-pub-4498940312772097/3149628958'
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
-    banner: 'ca-app-pub-6869992474017983/4806197152',
-    interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    banner: 'ca-app-pub-4498940312772097/6475832156',
+    interstitial: 'ca-app-pub-4498940312772097/3149628958'
   };
 } else {
   admobid = { // for Windows Phone
-    banner: 'ca-app-pub-6869992474017983/8878394753',
-    interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    banner: 'ca-app-pub-4498940312772097/6475832156',
+    interstitial: 'ca-app-pub-4498940312772097/3149628958'
   };
 }
 
@@ -24,7 +24,7 @@ function initApp() {
   // this will create a banner on startup
   AdMob.createBanner( {
     adId: admobid.banner,
-    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    position: AdMob.AD_POSITION.TOP_CENTER,
     isTesting: true, // TODO: remove this line when release
     overlap: false,
     offsetTopBar: false,
